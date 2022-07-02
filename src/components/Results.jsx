c7b46355efe9f4fcfd8bd8b182a9780c17dd9bbb
Marco Chávez <201020831@ingenieria.usac.edu.gt>
2022-07-02 17:50:12 -0600

    Regresion polinomial implementado

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,31 +1,70 @@
 import { Card, CardContent, CardHeader, Typography } from '@mui/material';
 
-/**
- * Retorna la imagen a mostrar en el resultado o una imagen por defecto
- * @param {String} imageB64 Base 64 de la imagen
- * @returns Base 64 o URL de la imagen a mostrar
- */
-const getImage = (imageB64) => {
-	imageB64 === ''
-		? 'https://i.pinimg.com/474x/5a/61/30/5a613089c64fa51c2c17877c030d4eed.jpg'
-		: (imageB64 = imageB64.slice(2, -1));
-};
-
 export const Results = ({ func, pred, imageB64 }) => {
 	return (
 		<Card sx={{ minWidth: 275 }}>
 			<CardHeader title='Resultados' />
 			<CardContent>
-				<pre>{func}</pre>
-				<Typography>{pred}</Typography>
-				<img
-					src={
-						imageB64 === ''
-							? 'https://i.pinimg.com/474x/5a/61/30/5a613089c64fa51c2c17877c030d4eed.jpg'
-							: imageB64.slice(2, -1)
-					}
-					alt='Graph Result'
-				/>
+				<div>
+					<Typography>Función de tendencia: </Typography>
+					<pre>{func}</pre>
+				</div>
+				<div
+					style={{
+						display: 'flex',
+						placeContent: 'flex-start',
+						placeItems: 'center',
+						gap: 8,
+					}}>
+					<Typography>Predicción: </Typography>
+					<pre>{pred}</pre>
+				</div>
+				<div>
+					{imageB64 && (
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'space-around',
+							}}>
+							<p>
+								<span
+									style={{
+										background: '#1594AD',
+										color: '#1594AD',
+									}}
+									Labels
+									graph>
+									-----
+								</span>{' '}
+								Plot
+							</p>
+							<p>
+								<span
+									style={{
+										background: '#AD5203',
+										color: '#AD5203',
+									}}
+									Labels
+									graph>
+									-----
+								</span>{' '}
+								Tendencia
+							</p>
+						</div>
+					)}
+					<img
+						style={{ maxWidth: '60vw' }}
+						src={
+							imageB64 === ''
+								? 'https://i.pinimg.com/474x/5a/61/30/5a613089c64fa51c2c17877c030d4eed.jpg'
+								: `data:image/jpeg;base64,${imageB64.slice(
+										2,
+										-1
+								  )}`
+						}
+						alt='Graph Result'
+					/>
+				</div>
 			</CardContent>
 		</Card>
 	);
