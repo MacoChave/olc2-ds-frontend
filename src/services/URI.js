21876c257253a81d90dac07703802b6f5ce191fe
Marco Chávez <201020831@ingenieria.usac.edu.gt>
2022-07-04 16:16:31 -0600

    URI y Vite config

--- src/services/URI.js
+++ src/services/URI.js
@@ -1,2 +1,2 @@
-export const URI = 'http://127.0.0.1:5000'
-// export const URI = 'http://34.168.78.33:8080'
+// export const URI = 'http://127.0.0.1:5000'
+export const URI = 'http://34.168.78.33:8080'
